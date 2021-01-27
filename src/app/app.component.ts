2794389c4f55a7d0e0af3f159058e0737c437336
hailita1 <hailit2306@gmail.com>
2021-01-27 11:27:45 +0700

    oke

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import * as introJs from 'intro.js/intro.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  introJS = introJs();
+
+  constructor() {
+    this.introJS.setOptions({
+      steps: [
+        {
+          element: '#step1',
+          intro: 'Welcome to your new app!',
+          position: 'bottom'
+        },
+        {
+          element: '#step2',
+          intro: 'Ok, wasn\'t that fun?',
+          position: 'right'
+        },
+        {
+          element: '#step3',
+          intro: 'let\'s keep going',
+          position: 'top'
+        },
+        {
+          element: '#step4',
+          intro: 'More features, more fun.',
+          position: 'right'
+        }
+      ],
+      showProgress: true
+    });
+  }
+
+  ngOnInit(): void {
+    this.introJS.start();
+  }
+}
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,50 @@
+import {Component, OnInit} from '@angular/core';
+import * as introJs from 'intro.js/intro.js';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  introJS = introJs();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  startSteps(): void {
+    this.introJS
+      .setOptions({
+        steps: [
+          {
+            element: '#step1-li',
+            intro: 'Welcome to your new app!'
+          },
+          {
+            element: '#step2-li',
+            intro: 'Ok, wasn\'t that fun?'
+          },
+          {
+            element: '#step3-li',
+            intro: 'let\'s keep going'
+          },
+          {
+            element: '#step4-li',
+            intro: 'More features, more fun.'
+          },
+          {
+            // As you can see, thanks to the element ID
+            // I can set a step in an element in an other component
+            element: '#step1',
+            intro: 'Accessed and element in another component'
+          }
+        ],
+        hidePrev: true,
+        hideNext: false
+      })
+      .start();
+  }
+}
